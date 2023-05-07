c9aee8ce102cc0149424230b006887bee8b4d175
장용진 <130991633+jyj9111@users.noreply.github.com>
2023-05-07 16:04:41 +0900

    [Silver II] Title: 에디터, Time: 424 ms, Memory: 55176 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -3,61 +3,46 @@ import java.util.LinkedList;
 import java.util.ListIterator;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		String str = br.readLine();
-		int M = Integer.parseInt(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        LinkedList<Character> list = new LinkedList<Character>();
 
-		LinkedList<Character> list = new LinkedList<Character>();
+        String str = br.readLine();
 
-		for(int i = 0; i < str.length(); i++) {
-			list.add(str.charAt(i));
-		}
+        for (int i = 0; i < str.length(); i++) {
+            list.add(str.charAt(i));
+        }
+        ListIterator iter = list.listIterator();
+        while (iter.hasNext()) {
+            iter.next();
+        }
 
-		//iterator 메소드 호출 
-		ListIterator<Character> iter = list.listIterator();
-		//처음 커서는 문장의 맨 뒤에 있어야하기 때문에 커서를 맨뒤로 이동시켜줌 (ex. abc|)
-		while(iter.hasNext()) {
-			iter.next();
-		}
-	
-		for(int i = 0; i < M; i++) {
-			String command = br.readLine();
-			char c = command.charAt(0);
-			switch(c) {
-			case 'L':
-				if(iter.hasPrevious())
-					iter.previous();
+        int num = Integer.parseInt(br.readLine());
 
-				break;
-			case 'D':
-				if(iter.hasNext())
-					iter.next();
-
-				break;
-			case 'B':
-				//remove()는 next()나 previous()에 의해 반환된 가장 마지막 요소를 리스트에서 제거함
-				if(iter.hasPrevious()) {
-					iter.previous();
-					iter.remove();
-				}
-				break;
-			case 'P':
-				char t = command.charAt(2);
-				iter.add(t);
-
-				break;
-			default:
-				break;
-			}
-		}
-		for(Character chr : list) {
-			bw.write(chr);
-		}
-		
-		bw.flush();
-		bw.close(); 
-	}
+        for (int i = 0; i < num; i++) {
+            String mode = br.readLine();
+            if(mode.charAt(0) == 'P') {
+                iter.add(mode.charAt(2));
+            } else if (mode.charAt(0) == 'L') {
+                if(iter.hasPrevious())
+                    iter.previous();
+            } else if (mode.charAt(0) == 'D') {
+                if(iter.hasNext())
+                    iter.next();
+            } else if (mode.charAt(0) == 'B') {
+                if(iter.hasPrevious()) {
+                    iter.previous();
+                    iter.remove();
+                }
+            } else {
+                break;
+            }
+        }
+        for(Character chr : list) {
+            bw.write(chr);
+        }
+        bw.flush();
+        bw.close();
+    }
 }
