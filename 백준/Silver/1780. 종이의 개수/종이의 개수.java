754d4157d4cfa01c927c3f49a6302c206269a6e7
장용진 <130991633+jyj9111@users.noreply.github.com>
2023-05-21 21:34:56 +0900

    [Silver II] Title: 종이의 개수, Time: 904 ms, Memory: 320296 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.java
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.java
@@ -1,48 +1,56 @@
-import java.util.Scanner;
+import java.io.*;
 
-public class Main {
-    static int N;
+public class Main { //Boj 1780: 종이의 개수 (재귀)
     static int[][] paper;
-    static int[] cnt; // -1, 0, 1로 채워진 종이 갯수
+    static int num;
+    static int[] cnt = new int[3];
 
-    // 해당 종이 내부에 같은 숫자로만 채워졌는지 확인하는 함수
-    static boolean check(int x, int y, int n) {
-        for (int i = x; i < x + n; i++) {
+    static boolean isSame(int x, int y, int n) {
+        for (int i = x; i < x + n ; i++) {
             for (int j = y; j < y + n; j++) {
-                if (paper[x][y] != paper[i][j]) {
+                if(paper[x][y] != paper[i][j])
                     return false;
-                }
             }
         }
         return true;
     }
 
-    static void solve(int x, int y, int z) {
-        if (check(x, y, z)) {
+    static void Paper(int x, int y, int z) {
+        if(isSame(x, y, z)) {
             cnt[paper[x][y] + 1] += 1;
             return;
         }
+
         int n = z / 3;
+
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                solve(x + i * n, y + j * n, n);
+                Paper(x + i * n, y + j * n, n);
             }
         }
     }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        N = scanner.nextInt();
-        paper = new int[N][N];
-        cnt = new int[3];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                paper[i][j] = scanner.nextInt();
+        num = Integer.parseInt(br.readLine());
+        paper = new int[num][num];
+
+        for (int i = 0; i < num; i++) {
+            String[] split = br.readLine().split(" ");
+            for (int j = 0; j < num; j++) {
+                paper[i][j] = Integer.parseInt(split[j]);
             }
         }
-        solve(0, 0, N);
-        for (int i = 0; i < 3; i++) {
-            System.out.println(cnt[i]);
+        Paper(0, 0, num);
+
+        for (int i = 0; i < cnt.length; i++) {
+            bw.append(Integer.toString(cnt[i]) + "\n");
         }
+
+        bw.flush();
+
+        br.close();
+        bw.close();
     }
 }
