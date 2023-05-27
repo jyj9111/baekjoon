9a040c19cf0e8d5dc05ebcf42efe88f68c7fa698
장용진 <130991633+jyj9111@users.noreply.github.com>
2023-05-27 22:51:17 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.03 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -1,6 +1,5 @@
 class Solution {
     public int solution(String str1, String str2) {
-        String repStr = str1.replace(str2, "*");
-        return repStr.contains("*") ? 1 : 2;
+        return str1.contains(str2) ? 1 : 2;
     }
 }
