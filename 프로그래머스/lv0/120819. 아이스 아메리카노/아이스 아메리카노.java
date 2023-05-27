592bed8ff0a816bf231158d464e7e760a0aac8c3
장용진 <130991633+jyj9111@users.noreply.github.com>
2023-05-27 21:15:19 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int money) {
+        
+        int coffee = money / 5500;
+        int change = money % 5500;
+        
+        
+        int[] answer = {coffee, change};
+        return answer;
+    }
+}
