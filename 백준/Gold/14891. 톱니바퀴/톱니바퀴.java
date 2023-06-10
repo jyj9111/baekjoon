f033383285599a63ecfb431a9d257c2b98d4e0cc
장용진 <130991633+jyj9111@users.noreply.github.com>
2023-06-11 00:05:23 +0900

    [Gold V] Title: 톱니바퀴, Time: 132 ms, Memory: 14532 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.Deque;
 
 public class Main { // Boj14891: 톱니바퀴 (시뮬레이션)
@@ -19,62 +18,49 @@ public class Main { // Boj14891: 톱니바퀴 (시뮬레이션)
         receiveInput();
 
         for (int i = 0; i < kNum; i++) {
-            rotateFlag = new boolean[3];
-
             int gear = data[i][0];
             int dir = data[i][1];
-
-            checkGearsPole();
-
-            if(gear == 1) {
-                rotateGear1(dir);
-                if(!rotateFlag[0]) continue;
-                rotateGear2(-dir);
-                if(!rotateFlag[1]) continue;
-                rotateGear3(dir);
-                if(rotateFlag[2]) rotateGear4(-dir);
-            } else if (gear == 2) {
-                rotateGear2(dir);
-                if(rotateFlag[0]) rotateGear1(-dir);
-                if(!rotateFlag[1]) continue;
-                rotateGear3(-dir);
-                if(rotateFlag[2]) rotateGear4(dir);
-
-            } else if (gear == 3) {
-                rotateGear3(dir);
-                if(rotateFlag[2]) rotateGear4(-dir);
-                if(!rotateFlag[1]) continue;
-                rotateGear2(-dir);
-                if(rotateFlag[0]) rotateGear1(dir);
-            } else {
-                rotateGear4(dir);
-                if(!rotateFlag[2]) continue;
-                rotateGear3(-dir);
-                if(!rotateFlag[1]) continue;
-                rotateGear2(dir);
-                if(rotateFlag[0]) rotateGear1(-dir);
+                                            
+            rotateFlag = new boolean[3];   
+            checkGearsPole();               
+                                            
+            switch (gear) {
+                case 1:
+                    rotateGear1(dir, true);
+                    break;
+                case 2:
+                    rotateGear2(dir, true);
+                    break;
+                case 3:
+                    rotateGear3(dir, true);
+                    break;
+                case 4:
+                    rotateGear4(dir, true);
+                    break;
             }
         }
 
         int sum = 0;
-        if(t1.getFirst() == 1) sum += t1.getFirst();
-        if(t2.getFirst() == 1) sum += t2.getFirst() * 2;
-        if(t3.getFirst() == 1) sum += t3.getFirst() * 4;
-        if(t4.getFirst() == 1) sum += t4.getFirst() * 8;
+        sum += t1.getFirst();
+        sum += t2.getFirst() * 2;
+        sum += t3.getFirst() * 4;
+        sum += t4.getFirst() * 8;
 
-        System.out.println(sum);
+        bw.append(sum + "");
+        bw.flush();
+        bw.close();
     }
 
     private static void checkGearsPole() {
-        int t1Tot2 = 0;
+        int t1Tot2 = 0; 
         int idx = 0;
         for(int temp : t1) {
             if(idx == 2) t1Tot2 = temp;
             idx++;
         }
 
-        int t2Tot1 = 0;
-        int t2Tot3 = 0;
+        int t2Tot1 = 0; 
+        int t2Tot3 = 0; 
         idx = 0;
         for(int temp : t2) {
             if(idx == 2) t2Tot3 = temp;
@@ -82,8 +68,8 @@ public class Main { // Boj14891: 톱니바퀴 (시뮬레이션)
             idx++;
         }
 
-        int t3Tot2 = 0;
-        int t3Tot4 = 0;
+        int t3Tot2 = 0; 
+        int t3Tot4 = 0; 
         idx = 0;
         for(int temp : t3) {
             if(idx == 2) t3Tot4 = temp;
@@ -91,12 +77,13 @@ public class Main { // Boj14891: 톱니바퀴 (시뮬레이션)
             idx++;
         }
 
-        int t4Tot3 = 0;
+        int t4Tot3 = 0; 
         idx = 0;
         for(int temp : t4) {
             if(idx == 6) t4Tot3 = temp;
             idx++;
         }
+        
         if(t1Tot2 != t2Tot1) {
             rotateFlag[0] = true;
         }
@@ -108,56 +95,68 @@ public class Main { // Boj14891: 톱니바퀴 (시뮬레이션)
         }
     }
 
-    private static void rotateGear1(int dir) {
-        if(dir == 1)
+    private static void rotateGear1(int dir, boolean mode) {
+        if(dir == 1) 
             t1.addFirst(t1.removeLast());
-        else
+        else        
             t1.addLast(t1.removeFirst());
+
+        if(mode) {  
+            if(!rotateFlag[0]) return;
+            rotateGear2(-dir, false);
+            if(!rotateFlag[1]) return;
+            rotateGear3(dir, false);
+            if(rotateFlag[2]) rotateGear4(-dir, false);
+        }
     }
 
-    private static void rotateGear2(int dir) {
+    private static void rotateGear2(int dir, boolean mode) {
         if(dir == 1)
             t2.addFirst(t2.removeLast());
         else
             t2.addLast(t2.removeFirst());
+
+        if(mode) {
+            if(rotateFlag[0]) rotateGear1(-dir, false);
+            if(!rotateFlag[1]) return;
+            rotateGear3(-dir, false);
+            if(rotateFlag[2]) rotateGear4(dir, false);
+        }
     }
 
-    private static void rotateGear3(int dir) {
+    private static void rotateGear3(int dir, boolean mode) {
         if(dir == 1)
             t3.addFirst(t3.removeLast());
         else
             t3.addLast(t3.removeFirst());
+
+        if(mode) {
+            if(rotateFlag[2]) rotateGear4(-dir, false);
+            if(!rotateFlag[1]) return;
+            rotateGear2(-dir, false);
+            if(rotateFlag[0]) rotateGear1(dir, false);
+        }
     }
 
-    private static void rotateGear4(int dir) {
+    private static void rotateGear4(int dir, boolean mode) {
         if(dir == 1)
             t4.addFirst(t4.removeLast());
         else
             t4.addLast(t4.removeFirst());
-    }
 
-    private static void printAll() {
-        for(int temp : t1) {
-            System.out.print(temp);
+        if(mode) {
+            if(!rotateFlag[2]) return;
+            rotateGear3(-dir, false);
+            if(!rotateFlag[1]) return;
+            rotateGear2(dir, false);
+            if(rotateFlag[0]) rotateGear1(-dir, false);
         }
-        System.out.println();
-        for(int temp : t2) {
-            System.out.print(temp);
-        }
-        System.out.println();
-        for(int temp : t3) {
-            System.out.print(temp);
-        }
-        System.out.println();
-        for(int temp : t4) {
-            System.out.print(temp);
-        }
-        System.out.println();
     }
 
     private static void receiveInput() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split("");
+
         for (int i = 0; i < 8; i++) {
             t1.add(Integer.parseInt(input[i]));
         }
