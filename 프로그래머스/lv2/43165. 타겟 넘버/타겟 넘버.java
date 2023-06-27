98461407c5ded24ab7f997d34e22c8e2cd53e45e
장용진 <130991633+jyj9111@users.noreply.github.com>
2023-06-27 10:41:04 +0900

    [level 2] Title: 타겟 넘버, Time: 0.52 ms, Memory: 66.6 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,33 +1,18 @@
 class Solution {
-    static int[] arr;
-    static int count = 0;
-
     public int solution(int[] numbers, int target) {
-        arr = new int[numbers.length];
-
-        backTracking(numbers, target,0);
-
-        return count;
+        dfs(numbers, 0, target, 0);
+        return answer;
     }
-    private void backTracking(int[] numbers, int target, int num) {
-        if(num == arr.length) {
-            calculate(numbers, target, arr);
+
+    private int answer = 0;
+
+    public void dfs(int[] numbers, int next, int target, int sum) {
+        if(next == numbers.length) {
+            if(sum == target) this.answer++;
             return;
         }
 
-        for (int i = 0; i < 2; i++) {
-            arr[num] = i;
-            backTracking(numbers, target, num + 1);
-        }
-
-    }
-
-    private void calculate(int[] numbers, int target, int[] arr) {
-        int sum = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            if(arr[i] == 0) sum += numbers[i];
-            else sum -= numbers[i];
-        }
-        if(sum == target) count++;
+        dfs(numbers, next + 1, target, sum + numbers[next]);
+        dfs(numbers, next + 1, target, sum - numbers[next]);
     }
 }
