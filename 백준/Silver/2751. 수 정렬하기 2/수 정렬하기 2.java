c2d1b39920d28efe91f746666b01f3f086da2de9
장용진 <130991633+jyj9111@users.noreply.github.com>
2023-07-21 22:04:21 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 900 ms, Memory: 101060 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,20 +1,52 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main { // Boj2751: 수 정렬하기 2
+    private int[] arr;
+    private int[] temp;
+
+    private void merge(int st, int ed) {
+        int mid = (st + ed) / 2;
+        int leftIdx = st;
+        int rightIdx = mid;
+
+        for (int i = st; i < ed; i++) {
+            if (rightIdx == ed) temp[i] = arr[leftIdx++];
+            else if (leftIdx == mid) temp[i] = arr[rightIdx++];
+            else if (arr[leftIdx] <= arr[rightIdx]) temp[i] = arr[leftIdx++];
+            else temp[i] = arr[rightIdx++];
+        }
+
+        for (int i = st; i < ed; i++) {
+            arr[i] = temp[i];
+        }
+    }
+
+    public void mergeSort(int st, int ed) {
+        if (ed == st + 1) return;
+        int mid = (st + ed) / 2;
+        mergeSort(st, mid);
+        mergeSort(mid, ed);
+        merge(st, ed);
+    }
+    
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int nNum =  Integer.parseInt(reader.readLine());
 
-        int[] arr =  new int[nNum];
+        Main obj = new Main();
+        obj.arr =  new int[nNum];
+        obj.temp =  new int[nNum];
+
         for (int i = 0; i < nNum; i++) {
-            arr[i] = Integer.parseInt(reader.readLine());
+            obj.arr[i] = Integer.parseInt(reader.readLine());
         }
-        Arrays.sort(arr);
+        
+        obj.mergeSort(0, nNum);
+        
         StringBuilder answer = new StringBuilder();
-        for (int i : arr) {
+        for (int i : obj.arr) {
             answer.append(i).append('\n');
         }
         System.out.println(answer);
