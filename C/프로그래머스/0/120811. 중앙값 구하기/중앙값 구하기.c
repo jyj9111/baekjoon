f087b6e5c667a9df90840819d00f97f501bbf10f
장용진 <130991633+jyj9111@users.noreply.github.com>
2024-04-11 08:43:46 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.01 ms, Memory: 4.09 MB -BaekjoonHub

---
+++ C/프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int compare(const int* num1, const int* num2)
+{
+    return *num1 - *num2;
+}
+// array_len은 배열 array의 길이입니다.
+int solution(int array[], size_t array_len) {
+    int answer = 0;
+    qsort(array, array_len, sizeof(array[0]), compare);
+    return array[array_len / 2];
+}
