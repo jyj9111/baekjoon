e909953189a363856c362c44def7fd980e85377d
장용진 <130991633+jyj9111@users.noreply.github.com>
2024-04-25 16:14:28 +0900

    [level 0] Title: 합성수 찾기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ C/프로그래머스/0/120846. 합성수 찾기/합성수 찾기.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int answer = 0;
+    int cnt = 0;
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= i; j++) {
+            if(i % j == 0) cnt++;
+        }
+        if(cnt >= 3) answer++;
+        cnt = 0;
+    }
+    return answer;
+}
