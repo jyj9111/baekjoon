c04656e7aab7a61e259bf8438022059d17c91f99
장용진 <130991633+jyj9111@users.noreply.github.com>
2024-04-26 22:45:39 +0900

    [level 0] Title: 약수 구하기, Time: 0.01 ms, Memory: 4.23 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int* solution(int n) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int* answer = (int*)malloc(501);
+    int idx = 0;
+    for(int i = 1; i <= n; i++) {
+        if(n % i == 0) answer[idx++] = i;
+    }
+    return answer;
+}
