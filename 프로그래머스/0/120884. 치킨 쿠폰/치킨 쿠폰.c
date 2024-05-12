f56215322936cd4c352c441fb6d9b81a0a386687
장용진 <130991633+jyj9111@users.noreply.github.com>
2024-05-12 21:41:28 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.c
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-// 1999
-// 222
+
 int solution(int chicken) {
     int remain_coupon = 0;
     int service = 0;
@@ -14,24 +13,15 @@ int solution(int chicken) {
         cnt++;
         service += (chicken / 10);
         remain_coupon += (chicken % 10);
-        chicken /= 10;
-        // printf("%d: service = %d remain_cupon = %d\n", cnt, service, remain_coupon);
-        
+        chicken /= 10;  
     }
     int temp = remain_coupon / 10;
     if(temp != 0) {
         service += temp;
         remain_coupon %= 10;
-        // printf("remain : %d\n", remain_coupon);
         remain_coupon += temp;
         service += (remain_coupon / 10);
-        // printf("%d", service);
         return service;
     }
-    else {
-        // printf("%d", service);
-        return service;
-    }
-    
     return service;
 }
